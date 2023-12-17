d5b4b636f46df2dfc4ef558ce99f9195c11da2ce
Benny254 <asshimbenny01@mail.com>
2023-12-17 03:03:40 -0800

    update

--- 3-main.c
+++ 3-main.c
@@ -3,7 +3,7 @@
 #include "../sort.h"
 
 /**
- * main - The entry point
+ * main - Entry point
  *
  * Return: Always 0
  */
@@ -14,7 +14,7 @@ int main(void)
 
 	print_array(array, n);
 	printf("\n");
-	bubble_sort(array, n);
+	quick_sort(array, n);
 	printf("\n");
 	print_array(array, n);
 	return (0);
